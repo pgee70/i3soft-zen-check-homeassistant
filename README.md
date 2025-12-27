06aa21149abcf987217f9396b6933dccb965f3ba
Peter Gee <pgee@me.com>
2025-12-27 22:52:58 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# description
+project to turn off mysql slave status on the server
+when the power plug is no longer visible, or has a status of off
+turns the slave back on when the power plug status is on.
+
+## setup
+clone this repository
+```bash
+cp sample.env .env
+npm install
+```
---
+++ src/setMysqlSlaveStatus.js
@@ -0,0 +1,54 @@
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const dbHost = process.env.DB_HOST;
+const dbName = process.env.DB_NAME;
+const dbPass = process.env.DB_PASS;
+const dbUser = process.env.DB_USER;
+
+const setMysqlSlaveStatus = newStatus => {
+  const possibleStatuses = ['on', 'off'];
+  if (!possibleStatuses.includes(newStatus)) {
+    console.error(`Status: ${newStatus} is invalid.`);
+    return;
+  }
+  const connection = mysql.createConnection({
+    database: dbName,
+    host: dbHost,
+    password: dbPass,
+    user: dbUser,
+  });
+  const checkSql = "SELECT COUNT(1) SlaveThreads FROM information_schema.processlist WHERE user = 'system user';"
+  const slaveStatusSql = 'show slave status';
+
+  const sqlQuery = newStatus.toLowerCase() === 'on'
+    ? 'start slave'
+    : 'stop slave';
+  connection.connect(err => {
+    if (err) {
+      console.error('Error connecting to MySQL database:', err);
+      return;
+    }
+    connection.query(checkSql, (error, results) => {
+      if (error) throw error;
+      if (parseInt(results[0].SlaveThreads, 10) === 0) {
+        console.log('Mysql slave is not set up, returning');
+        return;
+      }
+      connection.query(slaveStatusSql, (error, results) => {
+        if (error) throw error;
+        const currentStatus = results[0].Slave_IO_Running;
+        if ((currentStatus === 'No' && newStatus === 'On')
+          || (currentStatus === 'Yes' && newStatus === 'Off')) {
+          connection.query(sqlQuery, (error, results) => {
+            if (error) throw error;
+          });
+        }
+      })
+    });
+    connection.end(); // Close the connection when done
+  });
+}
+export default setMysqlSlaveStatus;
